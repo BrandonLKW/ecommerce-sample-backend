86530193d32964a3a8487c0bcda1453373c072aa
Brandon Lum <lum371@hotmail.com>
2025-03-12 02:22:25 +0800

    init server file

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const path = require("path");
+
+const server: Express = express();
+
+//middleware block
+server.use(express.json());
+server.use(express.static(path.join(__dirname, "dist")));
+
+//routes block
+server.get("/test", (_req: Request, res: Response) => {
+    res.json({ hello: "world" });  
+});
+
+//listen block
+const port = process.env.PORT || 3000;
+server.listen(port, function () {
+    console.log(`Express app running on port ${port}`);
+});
